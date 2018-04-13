90128e121f808ce06bdddacaa38caacaf14c67ca
jeromesenyonga <jeromesenyonga4@gmail.com>
2018-04-13 07:12:12 -0700

    my first time

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.set('view engine', 'handlebars')
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('main');
 })
 
 app.listen(app.get('port'), function(){
-    console.log(`Jerome Server started at ${app.get('port')}`;
+    console.log(`Jerome Server started at ${app.get('port')}`);
 })
