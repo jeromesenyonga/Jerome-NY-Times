eef9960141ece4804a739400f148909fbf80b44e
jeromesenyonga <jeromesenyonga4@gmail.com>
2018-04-13 06:28:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var tbs = require('express-handlebars')
+var path = require('path');
+
+var app = express();
+app.set('port', (process.env.PORT || 8080))
+
+app.engine('handlebars', tbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+app.listen(app.get('port'), function(){
+    console.log(`Jerome Server started at ${app.get('port')}`;
+})
